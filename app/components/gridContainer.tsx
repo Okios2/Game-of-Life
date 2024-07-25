4d1aecb99e503dc70fe031723848b108e7b24fbc
Okios2 <rakso3003@gmail.com>
2024-07-25 12:02:40 +0200

    Adding start/stop button

--- app/components/gridContainer.tsx
+++ app/components/gridContainer.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import ActionButton from "./actionbutton";
 import GameBoard, {rows, cols} from "../components/grid";
@@ -14,15 +14,35 @@ const GridContainer = () => {
     const resetToQeenBeeGrid = () => gridRef.current?.setPattern(queenBeePattern);
     const resetToRandomGrid  = () => gridRef.current?.setPattern(randomPattern(rows, cols));
     const resetToTumblerGrid = () => gridRef.current?.setPattern(tumblerPattern);
+    const resetToBlank = () => gridRef.current?.setPattern([[]]);
     const setNextGeneration = () => gridRef.current?.nextGrid();
+
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
+    const [isRunning, setIsRunning] = useState(false);
+
+    const simulation = () => {
+        if (isRunning) {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+                intervalRef.current = null;
+            }
+        } else {
+            intervalRef.current = setInterval(() => {
+                gridRef.current?.nextGrid();
+            }, 100);
+        }
+        setIsRunning(!isRunning);
+    };
 
     return (
         <div>
             <div>
+                <ActionButton onClick={resetToBlank}  name="Clear Board"/>
                 <ActionButton onClick={resetToQeenBeeGrid}  name="Queen Bee Pattern"/>
-                <ActionButton onClick={resetToRandomGrid}  name="Random Pattern"/>
                 <ActionButton onClick={resetToTumblerGrid}  name="Tumbler Pattern"/>
+                <ActionButton onClick={resetToRandomGrid}  name="Random Pattern"/>
                 <ActionButton onClick={setNextGeneration}  name="Next Generation"/>
+                <ActionButton onClick={simulation} name={isRunning ? "Stop" : "Start"} />
             </div>
             <div className={styles.center}>
                 <GameBoard ref={gridRef} />
